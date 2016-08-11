691c7ad9b7626e17514b969b1e3bf34eddf06eab
Matt Whiteley <matt@whiteleydesigns.com>
2016-08-11 08:24:42 -0400

    ACF Theme Options & Admin CSS
    Move ACF Theme Options functionality to plugin and remove from base child theme.
    Enquee admin stylesheet for styles applied to the WP Dashboard.

--- core-functionality.php
+++ core-functionality.php
@@ -15,3 +15,26 @@ define( 'WD_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) );
 //require_once WD_PATH.'/lib/post-types/post-type-news.php';
 //require_once WD_PATH.'/lib/shortcodes/shortcode-news.php';
 //require_once WD_PATH.'/lib/taxonomies/taxonomy-news.php';
+
+//* Add CSS to WP admin dashboard
+function wd_admin_css() {
+    wp_register_style( 'add-admin-stylesheet', plugins_url( '/core-functionality-master/lib/admin/admin-css.css' ) );
+    wp_enqueue_style( 'add-admin-stylesheet' );
+}
+add_action( 'admin_enqueue_scripts', 'wd_admin_css' );
+
+//* ACF -- Add options page
+//if( function_exists('acf_add_options_page') ) {
+//	acf_add_options_page(array(
+//		'page_title' 	=> 'Theme Options',
+//		'menu_title'	=> 'Theme Options',
+//		'menu_slug' 	=> 'theme-options',
+//		'capability'	=> 'edit_posts',
+//		'position'     => '58.997', // Adds under Genesis options page
+//		'icon_url'     => 'dashicons-image-filter', // https://developer.wordpress.org/resource/dashicons/
+//		'redirect'	=> false
+//	));
+//}
+
+//* ACF -- Hide ACF Menu from WordPress Dashboard (uncomment to use)
+//add_filter('acf/settings/show_admin', '__return_false');
