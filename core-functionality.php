b8bfd40cfbe48da121dc3330faa3676662f5c965
Matt Whiteley <matt@whiteleydesigns.com>
2018-04-03 14:47:49 -0400

    Update core-functionality.php

--- core-functionality.php
+++ core-functionality.php
@@ -38,3 +38,22 @@ add_action( 'admin_enqueue_scripts', 'wd_admin_css' );
 
 //* ACF -- Hide ACF Menu from WordPress Dashboard (uncomment to use)
 //add_filter('acf/settings/show_admin', '__return_false');
+
+/**
+ * Gravity Forms Domain
+ *
+ * Adds a notice at the end of admin email notifications
+ * specifying the domain from which the email was sent.
+ *
+ * @param array $notification
+ * @param object $form
+ * @param object $entry
+ * @return array $notification
+ */
+function wd_gravityforms_domain( $notification, $form, $entry ) {
+	if( $notification['name'] == 'Admin Notification' ) {
+		$notification['message'] .= 'Sent from ' . home_url();
+	}
+	return $notification;
+}
+add_filter( 'gform_notification', 'wd_gravityforms_domain', 10, 3 );
