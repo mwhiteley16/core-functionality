20cef7970acd7c5c57657ade46bfaa2ea679f596
Matt Whiteley <matt@whiteleydesigns.com>
2021-02-17 08:59:11 -0500

    Modified Separator Block
    Allow for dashed and dotted borders and use editor-color-palette to define border color.

--- lib/blocks/acf-blocks/templates/block-acf-separator.php
+++ lib/blocks/acf-blocks/templates/block-acf-separator.php
@@ -8,31 +8,46 @@
  * @license      GPL-2.0+
 **/
 
-// ACF custom fields
+// ACF Variables
 $wd_background_color = get_field( 'wd_background_color' );
+$wd_block_separator_type = get_field( 'wd_block_separator_type' );
 $wd_block_separator_width_choice = get_field( 'wd_block_separator_width_choice' );
 $wd_block_separator_width = get_field( 'wd_block_separator_width' );
 $wd_block_separator_height = get_field( 'wd_block_separator_height' );
 $wd_block_separator_include_margin = get_field( 'wd_block_separator_include_margin' );
 $wd_block_separator_alignment = get_field( 'wd_block_separator_alignment' );
 
-// block ID
+// get block editor colors
+$colors = get_theme_support( 'editor-color-palette' );
+foreach( $colors[0] as $color ) {
+
+     // loop over block editor colors and compare to color setting
+     if( $wd_background_color == $color['slug'] ) {
+          $border_color = $color['color'];  // set border color
+     }
+}
+
+// Block ID
 $block_id = 'acf-separator-' . $block['id'];
 
-// block Classes
+// Block Classes
 $block_classes = 'acf-block acf-separator-block ' . $wd_block_separator_include_margin . ' ' . $wd_block_separator_alignment;
 
+// get align class if present
 if( ! empty( $block['align'] ) ) {
      $block_classes .= ' align' . $block['align'];
 }
 
+// get custom class name if present
 if( ! empty( $block['className'] ) ) {
      $block_classes .= ' ' . $block['className'];
 }
 
 // set hr styles
-$hr_styles = '';
-$hr_styles .= 'height:' . $wd_block_separator_height . 'px;';
+$hr_styles = 'height: 1px;';
+$hr_styles .= 'border-top-style: ' . $wd_block_separator_type . ';';
+$hr_styles .= 'border-width: ' . $wd_block_separator_height . 'px;';
+$hr_styles .= 'border-color: ' . $border_color . ';';
 
 if( $wd_block_separator_width_choice == 'user-defined' ) {
      $hr_styles .= 'width:' . $wd_block_separator_width . 'px;';
@@ -41,5 +56,5 @@ if( $wd_block_separator_width_choice == 'user-defined' ) {
 ?>
 
 <div id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $block_classes ); ?>">
-     <hr class="has-<?php echo $wd_background_color; ?>-background-color" style="<?php echo esc_attr( $hr_styles ); ?>">
+     <hr style="<?php echo esc_attr( $hr_styles ); ?>">
 </div>
